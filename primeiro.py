8cb696828adb41fae601104a426c14852926d8fe
matheus vinicius <matheus.mvrf@gmail.com>
2025-07-12 04:21:42 -0300

    Add files via upload

---
+++ primeiro.py
@@ -0,0 +1,15 @@
+input=2
+
+input2=3
+
+adicao=(input + input2)
+print(adicao)
+
+subtracao=(input - input2)
+print(subtracao)
+
+multiplicacao=(input * input2)
+print(multiplicacao)
+
+divisao=(input / input2)
+print(divisao)
